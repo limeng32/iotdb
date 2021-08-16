c46b7dce94e9fc4cdf6cf19da547d7878a837f83
limeng32 <limengag@yonyou.com>
2021-08-16 15:37:45 +0800

    加入pojo代码（加入raw代码；将基本类型改为封装类型；加入getter、setter；加入pojoHelper；加入接口；加入orm注解；改造为LinkedHashMap；）；

---
+++ iotdb-ui/src/test/resources/db/schema.iotdb-ui.dev.sql
@@ -0,0 +1,50 @@
+--
+-- Licensed to the Apache Software Foundation (ASF) under one
+-- or more contributor license agreements.  See the NOTICE file
+-- distributed with this work for additional information
+-- regarding copyright ownership.  The ASF licenses this file
+-- to you under the Apache License, Version 2.0 (the
+-- "License"); you may not use this file except in compliance
+-- with the License.  You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing,
+-- software distributed under the License is distributed on an
+-- "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+-- KIND, either express or implied.  See the License for the
+-- specific language governing permissions and limitations
+-- under the License.
+--
+
+drop table if exists connect;
+
+drop table if exists query;
+
+drop table if exists user;
+
+create table tb_connect
+(
+   id                   bigint ,
+   host                 varchar(128) ,
+   port                 integer ,
+   username             varchar(20) ,
+   password             varchar(20) ,
+   alias                varchar(100) ,
+   user_id              bigint 
+);
+
+create table tb_query
+(
+   id                   bigint ,
+   name                 varchar(100) ,
+   sqls                 varchar(10000) ,
+   connect_id           bigint comment 
+);
+
+create table tb_user
+(
+   id                   bigint ,
+   name                 varchar(20) ,
+   password             varchar(200) 
+);
