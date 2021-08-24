082893056860ff391923ef9dd0b89d5feb6659e9
limeng32 <limengag@yonyou.com>
2021-08-24 12:47:12 +0800

    完成简单的登录功能；

--- iotdb-ui/src/main/java/org/apache/iotdb/ui/controller/UserController.java
+++ iotdb-ui/src/main/java/org/apache/iotdb/ui/controller/UserController.java
@@ -26,6 +26,8 @@ import org.apache.iotdb.ui.mapper.ConnectDao;
 import org.apache.iotdb.ui.mapper.UserDao;
 import org.apache.iotdb.ui.model.BaseVO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,11 +35,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
+@CrossOrigin
 @RestController
 @Api(value = "登录相关接口")
 public class UserController {
@@ -47,6 +51,9 @@ public class UserController {
 
 	@Autowired
 	private ConnectDao connectDao;
+
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
 
 	@ApiOperation(value = "errorTest", notes = "test")
 	@GetMapping(value = "/error/{errorName}")
@@ -70,12 +77,60 @@ public class UserController {
 
 	@ApiOperation(value = "/api/login/account", notes = "/api/login/account")
 	@RequestMapping(value = "/api/login/account", method = { RequestMethod.GET, RequestMethod.POST })
-	public BaseVO<JSONObject> loginAccount() {
+	public BaseVO<JSONObject> loginAccount(@RequestParam("username") String username,
+			@RequestParam("password") String password) {
 		JSONObject json = new JSONObject();
-		json.put("status", "ok");
-		json.put("type", "account");
-		json.put("currentAuthority", "admin");
-		BaseVO<JSONObject> ret = BaseVO.success(json);
+		if (username == null) {
+			json.put("status", "error");
+			json.put("type", "account");
+			json.put("currentAuthority", "guest");
+			return new BaseVO<JSONObject>("1", "用户名不能为空", json);
+		}
+		User u = new User();
+		u.setName(username);
+		User user = userDao.selectOne(u);
+		BaseVO<JSONObject> ret = null;
+		if (user != null && bCryptPasswordEncoder.matches(password, user.getPassword())) {
+			json.put("status", "ok");
+			json.put("type", "account");
+			json.put("currentAuthority", "admin");
+			ret = BaseVO.success(json);
+		} else {
+			json.put("status", "error");
+			json.put("type", "account");
+			json.put("currentAuthority", "guest");
+			ret = new BaseVO<JSONObject>("1", "找不到用户或用户名与密码不匹配", json);
+		}
 		return ret;
+	}
+
+	@ApiOperation(value = "/api/currentUser", notes = "/api/currentUser")
+	@RequestMapping(value = "/api/currentUser", method = { RequestMethod.GET, RequestMethod.POST })
+	public BaseVO<JSONObject> currentUser() {
+		JSONObject json = new JSONObject();
+		json.put("name", "李萌");
+		json.put("avatar", "https://gw.alipayobjects.com/zos/rmsportal/BiazfanxmamNRoxxVxka.png");
+		json.put("userid", "00000001");
+		json.put("email", "limengag@yonyou.com");
+		json.put("signature", "海纳百川，有容乃大");
+		json.put("title", "交互专家");
+		json.put("group", "蚂蚁金服－某某某事业群－某某平台部－某某技术部－UED");
+		json.put("tags", new JSONArray());
+		json.getJSONArray("tags").add(JSONObject.parseObject("{\"key\":\"0\",\"label\":\"很有想法的\"}"));
+		json.getJSONArray("tags").add(JSONObject.parseObject("{\"key\":\"1\",\"label\":\"专注设计\"}"));
+		json.getJSONArray("tags").add(JSONObject.parseObject("{\"key\":\"2\",\"label\":\"辣~\"}"));
+		json.getJSONArray("tags").add(JSONObject.parseObject("{\"key\":\"3\",\"label\":\"大长腿\"}"));
+		json.getJSONArray("tags").add(JSONObject.parseObject("{\"key\":\"4\",\"label\":\"川妹子\"}"));
+		json.getJSONArray("tags").add(JSONObject.parseObject("{\"key\":\"5\",\"label\":\"海纳百川\"}"));
+		json.put("notifyCount", 12);
+		json.put("unreadCount", 11);
+		json.put("country", "China");
+		json.put("geographic", new JSONObject());
+		json.getJSONObject("geographic").put("province",
+				JSONObject.parseObject("{\"key\":\"330000\",\"label\":\"浙江省\"}"));
+		json.getJSONObject("geographic").put("city", JSONObject.parseObject("{\"key\":\"330100\",\"label\":\"杭州市\"}"));
+		json.put("address", "西湖区工专路 77 号");
+		json.put("phone", "0752-268888888");
+		return BaseVO.success(json);
 	}
 }
